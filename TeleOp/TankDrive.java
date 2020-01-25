8bc8d2ed48886399720b0ba67cfd8318bdfe361e
Locust Valley Falconbots #7164 <57567227+FalconBots7164@users.noreply.github.com>
2020-01-25 11:58:06 -0500

    Create TankDrive.java

---
+++ TeleOp/TankDrive.java
@@ -0,0 +1,18 @@
+@TeleOp
+public class TankDrive extends OpMode {
+
+DcMotor m1;
+DcMotor m2;
+
+/* For four motor configuration
+DcMotor m3;
+DcMotor m4;
+*/
+
+@Override
+public void init() {}
+
+@Override
+public void loop() {}
+
+}
