47e3d0c4e65a2ce3e22704ea13d65b5cd8a17d4b
Locust Valley Falconbots #7164 <57567227+FalconBots7164@users.noreply.github.com>
2020-01-27 10:38:23 -0500

    Update TankDrive.java

--- TeleOp/TankDrive.java
+++ TeleOp/TankDrive.java
@@ -1,18 +1,54 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 @TeleOp
 public class TankDrive extends OpMode {
 
-DcMotor m1;
-DcMotor m2;
+  DcMotor m1;
+  DcMotor m2;
 
-/* For four motor configuration
-DcMotor m3;
-DcMotor m4;
-*/
+  //For four motor configuration, uncomment all m3/m4 code to use
+  /*
+  DcMotor m3;
+  DcMotor m4;
+  */
 
-@Override
-public void init() {}
+    @Override
+    public void init() {
 
-@Override
-public void loop() {}
+      m1 = hardwareMap.dcmotor.get("Motor 1");
+      m2 = hardwareMap.dcmotor.get("Motor 2");
+    
+      // Four motor config
+      /*
+      m3 = hardwareMap.dcmotor.get("Motor 3");
+      m4 = hardwareMap.dcmotor.get("Motor 4");
+      */
+      
+      // If you wish to change the direction the motors are using, you can choose to change it with this method.
+      // Be sure to uncomment the DcMotorSimple import statement above as well.
+      /*
+      m1.setDirection(DcMotorSimple.Direction.REVERSE);
+      m2.setDirection(DcMotorSimple.Direction.FORWARD);
+      */
+  
+    }
+
+    @Override
+    public void loop() {
+
+      m1.setPower(gamepad1.right_stick_y);
+      m2.setPower(gamepad1.left_stick_y);
+      
+      //Four motor config
+      /*
+      m3.setPower(gamepad1.right_stick_y);
+      m4.setPower(gamepad1.left_stick_y);
+      */
+
+    }
 
 }
