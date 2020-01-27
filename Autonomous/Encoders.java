a3c514af9afbc16b0912dd68f9699830d86a216e
Locust Valley Falconbots #7164 <57567227+FalconBots7164@users.noreply.github.com>
2020-01-27 11:13:03 -0500

    Update Encoders.java

--- Autonomous/Encoders.java
+++ Autonomous/Encoders.java
@@ -1,3 +1,7 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 @Autonomous
 public class Encoders extends LinearOpMode {
   DcMotor m1;
