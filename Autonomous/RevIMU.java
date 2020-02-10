6ff588599376a11ade871fdcf5e385eeab8a2d69
Locust Valley Falconbots #7164 <57567227+FalconBots7164@users.noreply.github.com>
2020-02-10 13:59:02 -0500

    Update RevIMU.java

--- Autonomous/RevIMU.java
+++ Autonomous/RevIMU.java
@@ -1,3 +1,12 @@
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 @Autonomous
 public class RevIMU extends LinearOpMode {
 
